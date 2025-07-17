cb0690bfec53eb2280ea1157193032a3ed400569
100Fernando100 <159192382+100Fernando100@users.noreply.github.com>
2025-07-17 13:46:25 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' https://assets.calendly.com https://calendly.com; object-src 'none';"
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
