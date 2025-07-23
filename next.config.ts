9eb8af3659f210cd81ae54641b63e2ef755e0e2b
Fernando <fernandosorokinl@gmail.com>
2025-07-23 14:40:20 -0400

    Update CSP headers to fix site loading

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,16 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "script-src 'self' https://assets.calendly.com https://calendly.com; object-src 'none';"
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdnjs.cloudflare.com https://assets.calendly.com https://calendly.com;
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https:;
+              font-src 'self' data:;
+              connect-src 'self' https:;
+              frame-src 'self' https://calendly.com;
+              object-src 'none';
+            `.replace(/\s{2,}/g, ' ').trim()
           }
         ]
       }
