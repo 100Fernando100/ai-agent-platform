62367d537f7fdb65f4f8111d0e75429f079d8043
Fernando <fernandosorokinl@gmail.com>
2025-07-23 17:50:25 -0400

    Remove CSP completely for testing

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdnjs.cloudflare.com https://assets.calendly.com https://calendly.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:; frame-src 'self' https://calendly.com; object-src 'none';"
-          }
-        ]
-      }
-    ]
-  }
 };
 
 export default nextConfig;
